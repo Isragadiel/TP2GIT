fd67b99c44be7b38f94d60b6fbccd407f6eac2a7
Israel <isragadiel@hotmail.com>
2025-04-02 01:09:58 -0300

    final

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,6 @@
-nombre="ariel"
-apellido="gonzalez"
+#se define variable nueva 
+nombre=input("Ingrese su nombre: ")
+#se define variable nueva 
+apellido=input("Ingrese su apellido: ")
+#se imprime el mensaje con las variables
 print(f"Hola {nombre} {apellido}, bienvenido a Python")
